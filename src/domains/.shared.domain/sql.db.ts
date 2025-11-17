ea04def76ba3dfda939a33f20842e74e9c34433f
riorichard98 <riorichard12@gmail.com>
2025-11-17 10:14:01 +0700

    feat: simplified sql.db.ts

--- src/domains/.shared.domain/sql.db.ts
+++ src/domains/.shared.domain/sql.db.ts
@@ -1,6 +1,3 @@
-export type SqlParams = readonly string[]
-export type SqlStatement = readonly [string, SqlParams]
-
 export default interface SqlDB {
     command(sql: string, params?: string[]): Promise<void>
     query(sql: string, params?: string[]): Promise<unknown>

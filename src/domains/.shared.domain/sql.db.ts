6e73e5209e62c8d681ff80816f231b448bebffdb
riorichard98 <riorichard12@gmail.com>
2025-11-17 08:57:13 +0700

    fix: simplified sql db domain

--- src/domains/.shared.domain/sql.db.ts
+++ src/domains/.shared.domain/sql.db.ts
@@ -2,7 +2,6 @@ export type SqlParams = readonly string[]
 export type SqlStatement = readonly [string, SqlParams]
 
 export default interface SqlDB {
-    command(statement: SqlStatement): Promise<void>
-    query(statement: SqlStatement): Promise<unknown>
-    prepareSql(sql: string, params?: string[]): SqlStatement 
+    command(sql: string, params?: string[]): Promise<void>
+    query(sql: string, params?: string[]): Promise<unknown>
 }

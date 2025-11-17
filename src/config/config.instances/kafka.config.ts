cc835a76d0848b5a428234449916756783305afd
riorichard98 <riorichard12@gmail.com>
2025-11-17 18:21:09 +0700

    feat: add group id & client id in kafka config

--- src/config/config.instances/kafka.config.ts
+++ src/config/config.instances/kafka.config.ts
@@ -2,13 +2,18 @@ import { Config } from '../../.core.internal.framework/internal.framework'
 
 type KafkaStringKeys =
     | "KAFKA_BROKER_NODE"
+    | "KAFKA_CLIENT_ID"
+    | "KAFKA_GROUP_ID"
+
 
 const kafkaKeys: KafkaStringKeys[] = [
-    "KAFKA_BROKER_NODE"
+    "KAFKA_BROKER_NODE",
+    "KAFKA_CLIENT_ID",
+    "KAFKA_GROUP_ID"
 ]
 
 export type TKafkaConfig = {
-    [K in KafkaStringKeys]: 
+    [K in KafkaStringKeys]:
     K extends KafkaStringKeys ? string : never
 }
 
@@ -19,7 +24,9 @@ export default class KafkaConfig extends Config<KafkaStringKeys, never, never> {
 
     public getAllVars(): TKafkaConfig {
         return {
-            KAFKA_BROKER_NODE: this.GET_CONFIG_STRING("KAFKA_BROKER_NODE")
+            KAFKA_BROKER_NODE: this.GET_CONFIG_STRING("KAFKA_BROKER_NODE"),
+            KAFKA_CLIENT_ID: this.GET_CONFIG_STRING("KAFKA_CLIENT_ID"),
+            KAFKA_GROUP_ID: this.GET_CONFIG_STRING("KAFKA_GROUP_ID")
         }
     }
 }

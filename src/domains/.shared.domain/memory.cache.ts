b2e29a25045279081c7a4447a907932cc16fbb94
riorichard98 <riorichard12@gmail.com>
2025-11-17 19:00:50 +0700

    fix: data type should only accept num/str/boolean/object

--- src/domains/.shared.domain/memory.cache.ts
+++ src/domains/.shared.domain/memory.cache.ts
@@ -1,5 +1,5 @@
 export default interface MemoryCache {
     incrementWithTTL(key: string, ttlSecond: number): Promise<number>
-    setWithTTL<T>(key: string, data: T, ttlSecond: number): Promise<void>
+    setWithTTL(key: string, data: string | boolean | number | object, ttlSecond: number): Promise<void>
     get(key: string): Promise<string | null>
 }

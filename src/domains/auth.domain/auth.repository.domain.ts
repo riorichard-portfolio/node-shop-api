cf59c25763dc565376518a91525a64862a2d3987
riorichard98 <riorichard12@gmail.com>
2025-11-18 18:32:47 +0700

    feat: auth query repo domain done

--- src/domains/auth.domain/auth.repository.domain.ts
+++ src/domains/auth.domain/auth.repository.domain.ts
@@ -1,12 +1,3 @@
-export interface AuthRepository {
-    processSaveSession(sessionId:string,userId:string,expiredAt:number): Promise<void> // process session in login (publish)
+export interface AuthQueryRepository {
     isSessionValid(sessionId: string): Promise<boolean>
-}
-
-export interface AuthConsumerRepository {
-    /**
-    * Bulk insert sessions dengan format: [sessionId, userId, expiredAt][]
-    * @param sessions Array of [sessionId, userId, expiredAt]
-    */
-    bulkInsertSession(sessions: [string, string, number][]): Promise<void>
 }

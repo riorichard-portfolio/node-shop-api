1e71fe375507d7374f844bccf6bf5f1861899bfb
riorichard98 <riorichard12@gmail.com>
2025-11-18 18:20:16 +0700

    feat: seperate query & command sql db also implement type strong

--- src/domains/.shared.domain/sql.db.ts
+++ src/domains/.shared.domain/sql.db.ts
@@ -1,4 +1,18 @@
-export default interface SqlDB {
-    command(sql: string, params?: string[]): Promise<void>
-    query(sql: string, params?: string[]): Promise<unknown>
+type Param = string | number | boolean
+type ColumnType = 'string' | 'boolean' | 'number'
+
+export interface SqlQueryDB {
+    query<const T extends ColumnType[]>(
+        sql: string,
+        columnTypes: T,
+        params?: Param[]
+    ): Promise<{
+        [K in keyof T]: T[K] extends 'string' ? string :
+        T[K] extends 'number' ? number :
+        T[K] extends 'boolean' ? boolean : never
+    }[]>
+}
+
+export interface SqlCommandDB {
+    command(sql: string, params?: Param[]): Promise<void>
 }

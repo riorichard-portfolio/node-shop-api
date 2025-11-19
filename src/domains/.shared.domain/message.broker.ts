95d55c0df38d50f0a255871a3da66d7b037a9b70
riorichard98 <riorichard12@gmail.com>
2025-11-19 11:35:33 +0700

    feat: export type consumerHandler

--- src/domains/.shared.domain/message.broker.ts
+++ src/domains/.shared.domain/message.broker.ts
@@ -1,8 +1,10 @@
+export type ConsumerHandler = (messages: string[]) => Promise<void> | void
+
 export interface MQProducer {
     publish(messageValues: (string | number | boolean)[][], topic: string, ack?: boolean): Promise<void>;
 }
 
 export interface MQConsumer {
-    on(topic: string, handler: (messages: string[]) => Promise<void> | void): this
+    on(topic: string, handler: ConsumerHandler): this
     startConsumer(): Promise<void>
 }

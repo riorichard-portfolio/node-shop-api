bdbdaf569b5ee3f52210d05bf0eb80830f9b45f6
riorichard98 <riorichard12@gmail.com>
2025-11-25 16:36:32 +0700

    feat: add user mq topics for user

--- src/domains/.shared.domain/message.broker.topics.ts
+++ src/domains/.shared.domain/message.broker.topics.ts
@@ -1,5 +1,9 @@
-type MQTopics<T extends Record<string, string>> = T ;
+type MQTopics<T extends Record<string, string>> = T;
 
 export type AuthMQTopics = MQTopics<{
     SESSION_CREATED: string
+}>
+
+export type UserMQTopics = MQTopics<{
+    USER_REGISTERED: string
 }>

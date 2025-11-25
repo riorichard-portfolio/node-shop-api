db91679801d9d5f47be36ab61edbb939bee23588
riorichard98 <riorichard12@gmail.com>
2025-11-25 16:47:40 +0700

    feat: auth query repo validate session return immutable data

--- src/repositories/auth/auth.query.repository.ts
+++ src/repositories/auth/auth.query.repository.ts
@@ -1,9 +1,12 @@
-import { AuthQueryRepository as AuthQueryRepo } from "../../domains/auth.domain/auth.repository.domain";
+import { AuthQueryRepository as AuthQueryRepo, ValidateSessionData, ValidSession } from "../../domains/auth.domain/auth.repository.domain";
 
-import { SqlQueryDB } from "../../domains/.shared.domain/sql.db";
+import { QuerySchema, SqlQueryDB } from "../../domains/.shared.domain/sql.db";
+import { RepositoryResult } from "../../domains/.shared.domain/types";
 
-const checkSessionSql = "select 1 from sessions where session_id = $1"
-const sessionColumns: ['number'] = ['number']
+const checkSessionSql = `select 1 as "exists" from sessions where session_id = $1`
+const sessionSchema = {
+    exists: 'number'
+} as const satisfies QuerySchema
 
 export default class AuthQueryRepository implements AuthQueryRepo {
     private readonly sqlDb: SqlQueryDB
@@ -13,9 +16,23 @@ export default class AuthQueryRepository implements AuthQueryRepo {
         this.sqlDb = sqlDb
     }
 
-    public async isSessionValid(sessionId: string): Promise<boolean> {
-        const params = [sessionId]
-        const rows = await this.sqlDb.query(checkSessionSql, sessionColumns, params)
-        return rows.length > 0
+    public async validateSession(data: ValidateSessionData): Promise<RepositoryResult<ValidSession>> {
+        const params = [data.sessionId()]
+        const rows = await this.sqlDb.query(checkSessionSql, sessionSchema, params)
+        if (rows.length <= 0) {
+            return {
+                found: false,
+            }
+        }
+        return {
+            found: true,
+            data() {
+                return {
+                    isValid() {
+                        return true
+                    },
+                }
+            },
+        }
     }
 }

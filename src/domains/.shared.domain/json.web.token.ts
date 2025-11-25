491ab366ca93388ce90a3366620bd301b70869c7
riorichard98 <riorichard12@gmail.com>
2025-11-25 16:36:14 +0700

    feat: mapped return and also immutable value output

--- src/domains/.shared.domain/json.web.token.ts
+++ src/domains/.shared.domain/json.web.token.ts
@@ -1,9 +1,28 @@
 type JWTPayload = Record<string, string | number | boolean>
+export type JWTVerifiedResult =
+    | {
+        isExpired: true
+        isInvalid: false
+    }
+    | {
+        isExpired: false
+        isInvalid: true
+    }
+    | {
+        isExpired: false
+        isInvalid: false
+        payload(): JWTPayload
+    }
+
+export type GeneratedToken = {
+    value(): string
+}
+
 
 export interface JWTTokenGenerator {
-    generateJWT(payload: JWTPayload): string
+    generateJWT(payload: JWTPayload): GeneratedToken
 }
 
 export interface JWTTokenVerifier {
-    verifyJWT(token: string): JWTPayload | null
+    verifyJWT(token: string): JWTVerifiedResult
 }

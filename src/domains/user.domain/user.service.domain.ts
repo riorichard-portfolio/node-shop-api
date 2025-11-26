a8289b034aa3701b05d112b07d6105209847d9de
riorichard98 <riorichard12@gmail.com>
2025-11-26 08:44:00 +0700

    feat: remove invalid email in create new user failed

--- src/domains/user.domain/user.service.domain.ts
+++ src/domains/user.domain/user.service.domain.ts
@@ -5,16 +5,29 @@ export type CreateNewUserOk = {
 }
 
 export type CreateNewUserFailed =
-    | { type: 'INVALID_EMAIL', email: string }
     | { type: 'EMAIL_EXIST', email: string }
 
 export interface CreateNewUserData {
     email(): string
     password(): string
-    fullName(): string
+    fullname(): string
 }
 
+export interface FindUserByEmailData {
+    email(): string
+}
+
+export type FindUserByEmailOk = {
+    email(): string
+    userId(): string
+    fullname(): string
+    hashedPassword(): string
+}
+
+export type FindUserByEmailFailed =
+    | { type: 'USER_NOT_EXIST', email: string }
 
 export interface UserService {
     createNewUser(data: CreateNewUserData): Promise<ServiceResult<CreateNewUserOk, CreateNewUserFailed>>
+    findUserByEmail(data: FindUserByEmailData): Promise<ServiceResult<FindUserByEmailOk, FindUserByEmailFailed>>
 }
